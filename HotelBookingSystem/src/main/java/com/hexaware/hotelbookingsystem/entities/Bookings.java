62a9300263fb71184233d9ce5a2c4965874f8ee1
angaYadahalli <gangayadahalli953@gmail.com>
2024-11-17 11:23:21 +0530

    Cozy heaven

---
+++ HotelBookingSystem/src/main/java/com/hexaware/hotelbookingsystem/entities/Bookings.java
@@ -0,0 +1,15 @@
+package com.hexaware.hotelbookingsystem.entities;
+
+	import java.time.LocalDate;
+
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.JoinColumn;
+	import jakarta.persistence.ManyToOne;
+
+	@Entity
+	public class Bookings { 
+		
+		   
+
+}
